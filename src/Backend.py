edef4507fa9ffbee59e129a391830002bbc25540
Ding Ma <ding.ma@mail.mcgill.ca>
2019-05-09 13:11:16 -0400

    date input and error testing works

--- src/Backend.py
+++ src/Backend.py
@@ -1,3 +1,23 @@
-def test():
-    print("1111")
-    #aa
+# checks for date input errors
+def inputDate(date):
+    unformatattedDate = date.split("/")
+    year = unformatattedDate[0]
+    month = unformatattedDate[1]
+    day = unformatattedDate[2]
+
+    oddMonths = ("01", "03", "05", "07", "09", "11")
+    evenMonths = ("04", "06", "08", "10", "12")
+    if len(year) != 4 or len(month) != 2:
+        dateErrors()
+
+    if month == "02" and int(day) > 28:
+        dateErrors()
+
+    if month in oddMonths and int(day) > 31:
+        dateErrors()
+
+    if month in evenMonths and int(day) > 30:
+        dateErrors()
+
+def dateErrors():
+    print("Date format error enter them again")
--- src/UI.py
+++ src/UI.py
@@ -1,10 +1,20 @@
-from tkinter import *
-from src import Backend as bk
+import tkinter as tk
+import Backend as Bk
 
-bk.test()
-window = Tk()
+window = tk.Tk()
 window.title("Welcome")
 window.geometry('800x600')
-bk.test()
+dateLabel = tk.Label(window, text="Enter Date in YYYY/MM/DD format")
+dateLabel.grid(column=0, row=0)
+txt = tk.Entry(window, width=10)
+txt.grid(column=1, row=0)
 
+
+def dateClicked():
+    res1 = txt.get()
+    Bk.inputDate(res1)
+
+
+btn = tk.Button(window, text=" Enter", command=dateClicked)
+btn.grid(column=2, row=0)
 window.mainloop()
