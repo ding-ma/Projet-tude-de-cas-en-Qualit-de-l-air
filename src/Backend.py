9bc4ecb5289339b030d072dfbdd79a2c4681c34c
Ma <ding.ma@ec.gc.ca>
2019-05-10 14:44:13 -0400

    read/write gemmach files

--- src/Backend.py
+++ src/Backend.py
@@ -17,38 +17,83 @@ import os
 #     print(stderr.readlines)
 
 # checks for date input errors
-def inputDate(date):
-    unformatattedDate = date.split("/")
-    year = unformatattedDate[0]
-    month = unformatattedDate[1]
-    day = unformatattedDate[2]
+
+def inputStartDate(sDate):
+    global sYear
+    global sMonth
+    global sDay
+    unformatattedDate = sDate.split("/")
+    sYear = unformatattedDate[0]
+    sMonth = unformatattedDate[1]
+    sDay = unformatattedDate[2]
 
     oddMonths = ("01", "03", "05", "07", "09", "11")
     evenMonths = ("04", "06", "08", "10", "12")
 
-    if len(year) != 4 or len(month) != 2 or month > "12" or len(day) != 2:
+    if len(sYear) != 4 or len(sMonth) != 2 or sMonth > "12" or len(sDay) != 2:
         dateErrors()
-    elif month == "02" and int(day) > 28:
+    elif sMonth == "02" and int(sDay) > 28:
         dateErrors()
-    elif month in oddMonths and int(day) > 31:
+    elif sMonth in oddMonths and int(sDay) > 31:
         dateErrors()
-    elif month in evenMonths and int(day) > 30:
+    elif sMonth in evenMonths and int(sDay) > 30:
         dateErrors()
     else:
-        print(year, month, day)
+        print(sYear, sMonth, sDay)
 
+def inputEndDate(eDate):
+    global eYear
+    global eMonth
+    global eDay
+    unformatattedDate = eDate.split("/")
+    eYear = unformatattedDate[0]
+    eMonth = unformatattedDate[1]
+    eDay = unformatattedDate[2]
+
+    oddMonths = ("01", "03", "05", "07", "09", "11")
+    evenMonths = ("04", "06", "08", "10", "12")
+
+    if len(eYear) != 4 or len(eMonth) != 2 or eMonth> "12" or len(eDay) != 2:
+        dateErrors()
+    elif eMonth == "02" and int(day) > 28:
+        dateErrors()
+    elif eMonth in oddMonths and int(eDay) > 31:
+        dateErrors()
+    elif eMonth in evenMonths and int(eDay) > 30:
+        dateErrors()
+    else:
+        print(eYear, eMonth, eDay)
 
 def dateErrors():
     print("Date format error enter them again")
 
 
+# 2017"+year"",08,08,2017,08,08\n"
 def modelChosen(model):
     if model == "GEM-MACH":
         print("Using GEM-MACH")
+        file = open("gemmach", "w")
+        file.write(
+            "target = /space/hall1/sitestore/eccc/oth/airq_central/sair001/Ding_Ma\n"
+            "filter = copy\n"
+            "postprocess = nopost\n"
+            "date = "
+            # start
+            + sYear + "," + sMonth + "," + sDay +","
+            # end
+            + eYear + "," + eMonth + "," + eDay +
+            "\nbranche = operation.forecasts.mach\n"
+            "ext = 002,003,004,005,006,007\n"
+            "heure = 00\n"
+            "priority = online\n"
+            "inc = 1\n"
+            "#\n")
+
     else:
         print("Function not supported yet")
+
 
 def toolChosen(tool):
     if tool == "XRARC":
         print("Lanching XRARC...")
-        os.system('XRARC')
+    # os.system('XRARC')
--- src/UI.py
+++ src/UI.py
@@ -5,8 +5,11 @@ from tkinter import ttk
 
 def Clicked():
 
-    date = enteredDate.get()
-    Bk.inputDate(date)
+    sDate = enteredDate.get()
+    Bk.inputStartDate(sDate)
+
+    eDate = enteredEndDate.get()
+    Bk.inputEndDate(eDate)
 
     model = modelCombo.get()
     Bk.modelChosen(model)
@@ -18,11 +21,17 @@ window = tk.Tk()
 window.title("Welcome")
 window.geometry('800x600')
 
-# date
-dateLabel = tk.Label(window, text="Enter Date in YYYY/MM/DD format")
-dateLabel.grid(column=0, row=0)
+# Start date
+startDateLabel = tk.Label(window, text="Enter Start Date in YYYY/MM/DD format")
+startDateLabel.grid(column=0, row=0)
 enteredDate = tk.Entry(window, width=13)
 enteredDate.grid(column=1, row=0)
+
+# Start date
+endDateLabel = tk.Label(window, text="Enter End Date in YYYY/MM/DD format")
+endDateLabel.grid(column=0, row=1)
+enteredEndDate = tk.Entry(window, width=13)
+enteredEndDate.grid(column=1, row=1)
 
 # execute button
 btn = tk.Button(window, text=" Enter", command=Clicked, width=20,height=5)
@@ -30,25 +39,25 @@ btn.grid(column=10, row=10)
 
 # model
 modelLabel = tk.Label(window, text="Choose the model")
-modelLabel.grid(column=0, row=1)
+modelLabel.grid(column=0, row=2)
 modelCombo = ttk.Combobox(window,
                             values=[
                                 "GEM-MACH",
                                 "UMOS",
                                 "FireWork",
                                 "Others..."], state = 'readonly')
-modelCombo.grid(column=0, row=2)
+modelCombo.grid(column=0, row=3)
 modelCombo.current(0)
 
 # tools
 toolLabel = tk.Label(window, text="Choose the tool")
-toolLabel.grid(column=0,row=3)
+toolLabel.grid(column=0,row=4)
 toolCombo = ttk.Combobox(window,
                          values=[
                              "XRARC",
                              "RARC",
                              "Others..."], state = 'readonly')
-toolCombo.grid(column =0,row=4)
+toolCombo.grid(column =0,row=5)
 toolCombo.current(0)
 
 window.mainloop()
