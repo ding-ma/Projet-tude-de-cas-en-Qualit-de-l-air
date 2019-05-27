781a7edc9dd2bc0b92fee4403a7ea4964fa82e34
Ding Ma <ding.ma@mail.mcgill.ca>
2019-05-27 16:02:35 -0400

    EOD: working on sorting the file name in exel sheet
    Signed-off-by: Ding Ma <ding.ma@mail.mcgill.ca>

--- src/tests.py
+++ src/tests.py
@@ -1,205 +1,75 @@
-import csv
-import re
-import string
-import tkinter as tk
-from tkinter import ttk
+import os
 
-stationFile = open("stations_DB.csv", "r")
-reader = csv.reader(stationFile)
-stationList = list(reader)
+#
+#
+# days = ["05", "06"]
+# hours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20","21", "22", "23"]
+#
+# path = "/space/hall1/sitestore/eccc/oth/airq_central/sair001/Ding_Ma/config"
+# for root, dirs, files in os.walk(path):
+#     for file in files:
+#         os.remove(os.path.join(root, file))
+# for d in days:
+#     for h in hours:
+#         config = open("config/config"+d+"_"+h+".tcl", "w+")
+#         config.write(
+#             "set Data(SpLst)  \"O3\" \n"
+#             "set Data(TAG1)   \"TEST00.201904_201904_regeta\"\n"
+#             "set Data(TAG3)   \""+d+h+"\"\n"
+#             "set Data(outTXT)       \"SITE\" \n"
+#             "set Data(levels) \"76696048\"\n"  # todo confirm levels
+#             "set Data(MandatoryLevels) \"76696048\"\n"
+#             "set Data(Path)    /space/hall1/sitestore/eccc/oth/airq_central/sair001/Ding_Ma/bashtest/operation.forecasts.mach\n"
+#             "set Data(PathOut) /space/hall1/sitestore/eccc/oth/airq_central/sair001/Ding_Ma/bashtest/operation.forecasts.mach/out\n"
+#             "set Data(Start)      \"" + "2019" + "04" + "\"\n"
+#             "set Data(End)      \"" + "2019" + "04" + "\"\n"
+#             "set Data(Eticket)     \"RAQDPS020\"\n"
+#             "set Data(point) \"" + "Dorval" + "\"\n"
+#             "set Data(coord) \"" + "45.47" + " " + "-73.74" + "\"\n"
+#             "set Data(days) \"" + str(d) + "\"\n"  # todo confirm start day
+#             "set Data(hours) \""+ str(h) +"\"\n"
+#         )
 
-# creates list based on csv entry
-lstID = []
-lstName = []
-# lstDisplay is for the UI
-lstDisplay = []
-lstLatitude = []
-lstLongitude = []
-lstProvince = []
-for x in range(len(stationList)):
-    line = stationList[x]
-    ids = line[0]
-    name = line[1]
-    latitude = line[3]
-    longitude = line[4]
-    province = line[5]
-    lstDisplay.append(ids + ": " + name)
-    lstID.append(ids)
-    lstName.append(name)
-    lstLatitude.append(latitude)
-    lstLongitude.append(longitude)
-    lstProvince.append(province)
-
-
-# search algorithm
-def findWithStation(station):
-    index = isStationFound(string.capwords(station))
-    if index is False:
-        return "ID not found, invalid station name"
-    else:
-        # with the index, we can get the other values since all corresponding data are stored at the same index for the same value
-        stationID = lstID[index]
-        stationLongitude = lstLongitude[index]
-        stationLatitude = lstLatitude[index]
-        return string.capwords(station) + " (" + stationID + ") Lat: " + stationLatitude + " Lon: " + stationLongitude
-
-
-# returns the index of the item if it exist else, it returns false, gets around the item out of bound problem
-def isStationFound(StationInput):
-    if StationInput in lstName:
-        return lstName.index(StationInput)
-    return False
-
-
-# same logic for searching with ID
-def findWithID(ID):
-    index = isIDFound(ID)
-    if index is False:
-        return "Station name not found, invalid ID"
-    else:
-        stationName = lstName[index]
-        stationLongtitude = lstLongitude[index]
-        stationLatitude = lstLatitude[index]
-        return stationName + " (" + ID + ") Lat: " + stationLatitude + " Lon: " + stationLongtitude
+# # for a in os.listdir('config'):
+# #     os.system("./extract1.tcl "+"config/"+a)
+# #
+# # for b in os.listdir("/space/hall1/sitestore/eccc/oth/airq_central/sair001/Ding_Ma/bashtest/operation.forecasts.mach/out"):
+# #     aa = open("/space/hall1/sitestore/eccc/oth/airq_central/sair001/Ding_Ma/bashtest/operation.forecasts.mach/out/"+b,"r").read()
+# #     print(b)
+#
+#
+# # docName = []
+# # def removeEmptyFile(path):
+# #     docList = os.listdir(path)
+# #     for doc in docList:
+# #         docPath = os.path.join(path,doc)
+# #         if os.path.isfile(docPath):
+# #             if os.path.getsize(docPath)==0:
+# #                 os.remove(docPath)
+# #         if os.path.isdir(docPath):
+# #             removeEmptyFile(docPath)
+# # removeEmptyFile(r'/space/hall1/sitestore/eccc/oth/airq_central/sair001/Ding_Ma/bashtest/operation.forecasts.mach/out')
+#
 
 
-def isIDFound(IDinput):
-    if IDinput in lstID:
-        return lstID.index(IDinput)
-    return False
+a = ['config05_6.tcl', 'config06_12.tcl', 'config05_1.tcl', 'config06_7.tcl', 'config06_23.tcl', 'config06_17.tcl',
+     'config06_21.tcl', 'config06_14.tcl', 'config06_1.tcl', 'config05_23.tcl', 'config05_2.tcl', 'config06_15.tcl',
+     'config05_4.tcl', 'config06_10.tcl', 'config05_13.tcl', 'config06_8.tcl', 'config06_3.tcl', 'config06_2.tcl',
+     'config05_16.tcl', 'config06_20.tcl', 'config05_12.tcl', 'config05_14.tcl', 'config05_3.tcl', 'config05_15.tcl',
+     'config06_5.tcl', 'config05_0.tcl', 'config06_18.tcl', 'config05_22.tcl', 'config06_4.tcl', 'config05_11.tcl',
+     'config05_8.tcl', 'config05_9.tcl', 'config05_18.tcl', 'config06_11.tcl', 'config05_17.tcl', 'config05_21.tcl',
+     'config06_22.tcl', 'config06_9.tcl', 'config05_20.tcl', 'config05_7.tcl', 'config06_13.tcl', 'config06_19.tcl',
+     'config06_6.tcl', 'config06_0.tcl', 'config05_19.tcl', 'config05_10.tcl', 'config06_16.tcl', 'config05_5.tcl']
 
+days = ["05", "06"]
+hours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+         "20", "21", "22", "23"]
+hour = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17",
+        "18", "19", "20", "21", "22", "23"]
 
-# returning the found list
-def SearchNameID(userInput):
-    # ignores all digit
-    patten = re.compile('\D')
-    # if there are no digit, search by name. else, search with ID
-    if patten.findall(userInput):
-        return findWithStation(userInput)
-    else:
-        return findWithID(userInput)
-
-
-lstNL = []
-lstPEI = []
-lstNS = []
-lstNB = []
-lstQC = []
-lstONT = []
-lstMAN = []
-lstSASK = []
-lstALB = []
-lstBC = []
-lstYK = []
-lstNWT = []
-lstNVT = []
-j = 0
-for i in lstProvince:
-    if i == "1":
-        a = lstID[j]
-        lstNL.append(a)
-    if i == "2":
-        b = lstID[j]
-        lstPEI.append(b)
-    if i == "3":
-        c = lstID[j]
-        lstNS.append(c)
-    if i == "4":
-        d = lstID[j]
-        lstNB.append(d)
-    if i == "5":
-        q = lstID[j]
-        lstQC.append(q)
-    if i == "6":
-        w = lstID[j]
-        lstONT.append(w)
-    if i == "7":
-        e = lstID[j]
-        lstMAN.append(e)
-    if i == "8":
-        r = lstID[j]
-        lstSASK.append(r)
-    if i == "9":
-        t = lstID[j]
-        lstALB.append(t)
-    if i == "10":
-        y = lstID[j]
-        lstBC.append(y)
-    if i == "11":
-        u = lstID[j]
-        lstNWT.append(u)
-    if i == "12":
-        o = lstID[j]
-        lstYK.append(o)
-    if i == "13":
-        p = lstID[j]
-        lstNVT.append(p)
-    j = j + 1
-
-
-for m in lstQC:
-    indexofitem = lstID.index(m)
-    name = lstName[indexofitem]
-    ids = lstID[indexofitem]
-
-province = ['Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Nova Scotia',
-            'Ontario',
-            'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Northwest Territories', 'Nunavut', 'Yukon']
-
-provinceDic = {
-    "AB": lstALB,
-    "BC": lstBC,
-    "MB": lstMAN,
-    "NB": lstNB,
-    'NL': lstNL,
-    "NS": lstNS,
-    "ON": lstONT,
-    "PE": lstPEI,
-    "QC": lstQC,
-    "SK": lstSASK,
-    "NT": lstNWT,
-    "NU": lstNVT,
-    "YT": lstYK
-}
-
-testlst = []
-
-
-def gettingprovlist(prov):
-    for y in provinceDic[prov]:
-        indexofitem = lstID.index(y)
-        display = lstDisplay[indexofitem]
-        testlst.append(display)
-    return testlst
-
-
-window = tk.Tk()
-# window.attributes('-zoomed', True)
-w, h = window.winfo_screenwidth(), window.winfo_screenheight()
-
-
-# todo: optimize
-# optimize this thing, we should be able to get the k,v pair of the dictionary directly from the combobox but i dont know how
-# did a really big workaround for dynamically updating the other list too
-
-def vv(event):
-    testlst.clear()
-    aaaa = comboprov.get()
-    lsst = gettingprovlist(aaaa)
-    combostations.config(values=lsst)
-
-
-trylist = ["AB", "BC", "MB", "NB", 'NL', "NS", "ON", "PE", "QC", "SK", "NT", "NU", "YT"]
-
-comboprov = ttk.Combobox(window, values=trylist, width=10, state='readonly')
-comboprov.grid(column=2, row=2)
-comboprov.bind('<<ComboboxSelected>>', vv)
-comboprov.current(0)
-
-combostations = ttk.Combobox(window, values=lstDisplay, width=30, state='readonly')
-combostations.grid(column=3, row=2)
-combostations.current(1)
-
-window.geometry("%dx%d+0+0" % (w, h))
-
-window.mainloop()
+for d in days:
+    for p, h in zip(hours, hour):
+        file = open("out/" + d + h + ".txt", "w+")
+        file.write(p + d)
+a = os.listdir("out")
+print(a)
