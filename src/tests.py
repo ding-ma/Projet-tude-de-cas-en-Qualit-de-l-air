72a86035a28ea8ad2caf5663ee38ef978ab4f30e
Ding Ma <ding.ma@mail.mcgill.ca>
2019-06-06 15:59:32 -0400

    working on displaying gif on tkinter. found "convert" command on linux to change format from png to gif
    Signed-off-by: Ding Ma <ding.ma@mail.mcgill.ca>

--- src/tests.py
+++ src/tests.py
@@ -1,43 +1,13 @@
-import csv
-import difflib
-import re
+import tkinter as tk
 
-stationFile = open("stations.csv", "r")
-reader = csv.reader(stationFile)
-stationList = list(reader)
-# creates list based on csv entry
-lstID = []
-lstName = []
-#lstDisplay is for the UI
-lstDisplay = []
-lstLatitude = []
-lstLongitude = []
-lstProvince = []
-for x in range(len(stationList)):
-    line = stationList[x]
-    ids = line[0]
-    name = line[1]
-    latitude = line[3]
-    longitude = line[4]
-    province = line[5]
-    lstDisplay.append(ids + ": " + name)
-    lstID.append(ids)
-    lstName.append(name)
-    lstLatitude.append(latitude)
-    lstLongitude.append(longitude)
-    lstProvince.append(province)
+window = tk.Tk()
+# window.attributes('-zoomed', True)
+#w, h = window.winfo_screenwidth(), window.winfo_screenheight()
+w,h = 1225,890
+window.geometry("%dx%d+0+0" % (w, h))
+img = tk.PhotoImage(file = "aa.gif")
+imglabal = tk.Label(window,image = img)
+imglabal.grid(column=0, row=50, sticky='w')
 
-
-def findx(input):
-    i = 0
-    for r in lstName:
-        difflib.get_close_matches(input, str(r))
-        if re.search(input, r):
-            return i
-        i = i + 1
-
-while True:
-    a = difflib.get_close_matches(input(), lstName, n=1, cutoff=.4)
-    print(a)
-    # ids = lstName.index(a[0])
-    # print(lstID[ids])
+window.mainloop()
+#convert -delay 35 -loop 0 *.png aa.gif
