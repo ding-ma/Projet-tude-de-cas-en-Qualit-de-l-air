58ca81c86fa58d5729fe326cabdd00677c6c17e9
Ding Ma <ding.ma@mail.mcgill.ca>
2019-06-17 16:00:07 -0400

    playing with sql key value

--- src/tests.py
+++ src/tests.py
@@ -1,48 +1,10 @@
-import calendar
-from datetime import date, timedelta
+sqlTimedict = {
+    "00":(210000,220000,230000,0,10000,20000),
+    "06":(30000,40000,50000,60000,70000,80000),
+    "12":{"90000","100000","110000","120000","130000","140000"},
+    "18":(150000,160000,170000,180000,190000,200000)
+}
 
+#userInputTimedict = {v: k for k, v in sqlTimedict.items()}
 
-def aa():
-    sMonth = 2
-    eMonth = 3
-    startDate = date(2019,sMonth,5)
-    endDate = date(2019,eMonth,15)
-    lstsMonth = []
-    lsteMonth = []
-    lstDays = []
-    if sMonth is not eMonth:
-        a= calendar.monthrange(2019,sMonth)[1]
-        endMonth = date(2019,sMonth,a)
-        delta1 = endMonth-startDate
-        for f in range(delta1.days+1):
-            a = startDate+timedelta(days=f)
-            t = a.strftime("%d")
-            lstsMonth.append(t)
-
-        startMonth = date(2019,eMonth,1)
-        delta2 = endDate-startMonth
-        for q in range(delta2.days+1):
-            c = startMonth+timedelta(days=q)
-            w = c.strftime("%d")
-            lsteMonth.append(w)
-        return lstsMonth,lsteMonth
-    else:
-        delta = endDate-startDate
-        for a in range(delta.days+1):
-            e = startDate+timedelta(days=a)
-            v = e.strftime("%d")
-            lstDays.append(v)
-        return lstDays
-
-
-b = aa()
-if len(b) is 2:
-    a= ""
-    for l in b[1]:
-        a += l
-    for z in b[0]:
-        a +=z
-    print(a)
-else:
-    print(b)
-
+print(sqlTimedict["12"])
