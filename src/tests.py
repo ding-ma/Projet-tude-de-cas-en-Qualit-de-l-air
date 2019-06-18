11ad1db14603ffa8b8ec0361c64eeadf651e98a4
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2019-06-18 11:26:28 -0400

    works with multiple day and multiple species

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,9 @@
 import os
 import sqlite3 as sql
+import time
 from datetime import date, timedelta
+
+start = time.clock()
 
 sqlTimedict = {
     "00": (210000, 220000, 230000, 0, 10000, 20000),
@@ -30,31 +33,39 @@ sMonth = 6
 sDay = 11
 eYear = 2019
 eMonth = 6
-eDay = 11
+eDay = 14
 sDate = date(sYear,sMonth,sDay)
 eDate = date(eYear,eMonth,eDay)
 delta1 = eDate-sDate
 lstfile = []
+lstdays = []
+linuxpath = "/fs/site1/dev/eccc/oth/airq_central/sair001/Ding_Ma/ProjetQA/rarc/operation.observations.dbase.surface.airnow/"
+windowspath = "sql\\"
 for i in range(delta1.days+2):
     e=sDate+timedelta(days=i)
-    print(e)
-    for a in os.listdir("sql"):
+    lstdays.append(e)
+    for a in os.listdir(linuxpath):
         if a.startswith(e.strftime("%Y%m%d")):
             lstfile.append(a)
 
+
+species = ["1","2", "3"]
+stationID = "50128"
 for l in lstfile:
-    for s in ["1","2", "3"]:
-        stationID = "50128"
-        species = "1"
-        connection = sql.connect("sql\\"+l)
-        c = connection.cursor()
-        c.execute("SELECT COUNT(*) FROM (SELECT _rowid_,* FROM main.header)")
-        c.execute("SELECT _rowid_,* FROM main.header WHERE id_stn LIKE '%50128%'")
-        idObslst = []
-        for i in c.fetchall():
-            if i[6] == int(sDate.strftime("%Y%m%d")):  # range of dates
-                print("TIME: " + str(i[7]))
-                c.execute("SELECT COUNT(*) FROM (SELECT _rowid_,* FROM main.data)")
-                c.execute("SELECT _rowid_,* FROM main.data WHERE id_obs =" + str(i[1]) + " AND species =" + s)
-                for p in c.fetchall():
-                    print(s+"--"+str(p[8]))
+    for d in lstdays[:-1]:  # skips last date, but lstfile contains it so it reads it. we just need the last 3h of the eDate in the last file
+        for s in species:
+            connection = sql.connect(linuxpath+l)
+            c = connection.cursor()
+            c.execute("SELECT COUNT(*) FROM (SELECT _rowid_,* FROM main.header)")
+            c.execute("SELECT _rowid_,* FROM main.header WHERE id_stn LIKE '%50128%'")
+            idObslst = []
+            for i in c.fetchall():
+                if i[6] == int(d.strftime("%Y%m%d")):  # range of dates
+                    print("TIME: " + str(i[7])+" DATE: "+d.strftime("%Y%m%d"))
+                    c.execute("SELECT COUNT(*) FROM (SELECT _rowid_,* FROM main.data)")
+                    c.execute("SELECT _rowid_,* FROM main.data WHERE id_obs =" + str(i[1]) + " AND species =" + s)
+                    for p in c.fetchall():
+                        print(str((p[8]))+" species: "+s)
+
+end = time.clock()
+print(end-start)
