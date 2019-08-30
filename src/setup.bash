0c6bfb006d29a68f0580e9548db8850dbaaf3832
Ding Ma <ding.ma@mail.mcgill.ca>
2019-08-30 15:12:31 -0400

    added pip install for people that didnt have it

--- src/setup.bash
+++ src/setup.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+xlsxwritter="pip3 install xlsxwriter --user"
+echo "installing" $xlsxwritter
+eval $xlsxwritter
 shopt -s expand_aliases
 unzip="unzip files.zip"
 eval $unzip
 path=$(eval "pwd")
 str="alias AIDE=\x22cd $path/ && python3 -m UI.py\x22"
 ali=$(echo -e $str)
+# If script doesnt work, put the next line in comment as unix system should have $HOME defined.
 HOME=$(eval "cd")
 echo $ali>> $HOME/.profile.d/interactive/pre
